e494edae84eea7456b4f3fb5ccbebc1877ae352f
snowparsley <dhfpswlthd1234@naver.com>
2025-08-27 10:29:59 +0900

    메모사항

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/MY-APP" element={<Home />} />
         <Route path="/movie/:id" element={<MovieDetail />} />
       </Routes>
     </Router>
